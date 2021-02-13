20a2d97c2d9ed1a4f6a2abd50589cac246215bd1
Ayush <ayush@quovantis.com>
2021-02-13 15:16:25 +0530

    Committed In-memory authentication

---
+++ in-memory-athentication/src/main/java/com/self/inmemoryathentication/InMemoryAthenticationApplication.java
@@ -0,0 +1,15 @@
+package com.self.inmemoryathentication;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class InMemoryAthenticationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InMemoryAthenticationApplication.class, args);
+  }
+
+}
---
+++ in-memory-athentication/src/test/java/com/self/inmemoryathentication/InMemoryAthenticationApplicationTests.java
@@ -0,0 +1,15 @@
+package com.self.inmemoryathentication;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class InMemoryAthenticationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
