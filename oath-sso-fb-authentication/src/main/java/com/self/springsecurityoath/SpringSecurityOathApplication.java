2067459bc30096f4bdec22debcd8e9487e78968d
Ayush <ayush@quovantis.com>
2021-02-16 11:23:00 +0530

    Committed OAuth-SSO with facebook.

---
+++ oath-sso-fb-authentication/src/main/java/com/self/springsecurityoath/SpringSecurityOathApplication.java
@@ -0,0 +1,14 @@
+package com.self.springsecurityoath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringSecurityOathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOathApplication.class, args);
+	}
+
+}
---
+++ oath-sso-fb-authentication/src/test/java/com/self/springsecurityoath/SpringSecurityOathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.springsecurityoath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
